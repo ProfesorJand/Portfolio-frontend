758274cc63b21f5e267922ca648bd04e4505f566
Profesorjand <jorge.andrade.y@hotmail.com>
2022-12-10 17:56:40 -0300

    change default port to 80

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.post("/sendEmail", async (req,res)=>{
 
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT,()=>{
     console.log(`Server listening at port ${PORT}...`)
