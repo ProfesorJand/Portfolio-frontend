80fcc4c003e87cd3b3d1a232ebabf3d61ff51ddf
Profesorjand <jorge.andrade.y@hotmail.com>
2022-12-14 05:35:51 -0300

    adding style to proyects v1

--- portfolio/src/Components/Proyects/Proyects.jsx
+++ portfolio/src/Components/Proyects/Proyects.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Style from "./proyects.module.css";
 import axios from "axios";
+import { FaEye, FaStar, FaClone } from "react-icons/fa";
+import { BiGitRepoForked } from "react-icons/bi";
+
+import { IconContext } from "react-icons";
+//
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import {Carousel} from "react-responsive-carousel";
 
@@ -18,6 +23,7 @@ export default function Proyects() {
       .then((r) => setRepo(r));
     return;
   }
+
   return (
     <>
       <div className="tituloH2" id="proyects">
@@ -29,20 +35,54 @@ export default function Proyects() {
           repo.map((r) => {
             return (
               <div
-                className={Style.container}
+                className={Style.container + ` mySlides` + ` w3-animate-fading`}
                 key={r.id}
-                onClick={() => {
-                  window.open(r.html_url, "_blank");
-                }}
+                // onClick={() => {
+                //   window.open(r.html_url, "_blank");
+                // }}
               >
                 <h2>{r.name}</h2>
-                <img className={Style.img_thumbnail} src={`https://raw.githubusercontent.com/${r.full_name}/main/thumbnail.png`} alt="Repo thumbnail"/>
+                <img
+                  className={`${Style.img_thumbnail}`}
+                  src={`https://raw.githubusercontent.com/${r.full_name}/main/thumbnail.png`}
+                  alt="Repo thumbnail"
+                />
                 <p>{r.description}</p>
-                
+                <div className={Style.Info_Actions_Github}>
+                  <div>
+                    <FaStar></FaStar> {r.stargazers_count}
+                  </div>
+                  <div>
+                    <FaEye></FaEye> {r.watchers_count}
+                  </div>
+                  <div>
+                    <BiGitRepoForked></BiGitRepoForked> {r.forks_count}
+                  </div>
+                  {/* <div>
+                    <FaClone
+                      onClick={() => {
+                        const fork = document.getElementById(`${r.name}`);
+                        fork.select();
+                        document.execCommand("copy");
+                        console.log("copiado")
+                      }}
+                    ></FaClone>
+                <input type="text" value={r.clone_url} id={r.name} ></input>
+                  </div> */}
+                </div>
+
+                <input
+                  className={Style.InputGithub}
+                  value="Go Repository"
+                  type="button"
+                  onClick={() => {
+                    window.open(r.html_url, "_blank");
+                  }}
+                ></input>
               </div>
             );
           })}
-          {/* </Carousel> */}
+        {/* </Carousel> */}
       </div>
     </>
   );
